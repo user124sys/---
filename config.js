2a262a852bdd9b5b8c977bca8edd17330fb8ab69
user124sys <rehangeeneth2@gmail.com>
2025-05-06 23:17:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "", //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //Put Your Github Auth Token
+SESSION_ID: process.env.SESSION_ID || "SADIYA-MD=jE9EEIBT#1BF8GdVOdnanF6orVPo7jYEGl7Z2FQ4szW8mMQqT9Bg", //Put Session-id Here
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "user124sys", //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "nzYd3nZNwkBNI24buTS7Ec1ygNJn0E2WykiK", //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //Bot Database Repo Name
 SESSION_NAME: process.env.SESSION_NAME || "session"
 };
